6bb9187ee64b387aa685ec05ead34b681d948fa0
bruno7806 <130980270+bruno7806@users.noreply.github.com>
2023-04-17 13:11:01 +0200

    Create java.java

---
+++ java.java
@@ -0,0 +1,13 @@
+public class main {
+     public static void main(String[] args) {
+       
+         Saludo s1 = new Saludo ("Carlos");
+
+    Saludo s2 = new Saludo (null);
+
+    s1.saludar ("Luis");
+
+    Utilidades.saludar ("Ana");
+         
+    }
+}
